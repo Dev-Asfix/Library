be5948d49081911092104b95d77d2b5015a159fc
Dev Asfix <rosasramirez753@gmail.com>
2024-11-27 23:58:55 -0500

    commit

---
+++ inc/footer.php
@@ -0,0 +1,23 @@
+<footer class="footer full-reset">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <h4 class="all-tittles">Acerca de</h4>
+                <p>
+                Este es un sistema integrado de gestión informática que automatiza las diferentes actividades y procesos necesarios para el funcionamiento de una biblioteca: gestión de colecciones y de usuarios, catalogación, circulación, adquisiciones, consultas, estadísticas, informes, accesos al catálogo online, historial de préstamos, etc.
+                </p>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <h4 class="all-tittles">Desarrollador</h4>
+                <ul class="list-unstyled">
+                    <li>
+                        <i class="zmdi zmdi-check zmdi-hc-fw"></i>&nbsp; Pablo Rosas Ramirez
+                        <i class="" data-link=""></i>
+                        <i data-link="" class=""></i>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright full-reset all-tittles">© 2023 Pablito Rosas Ramirez todos los derechos reservados</div>
+</footer>
